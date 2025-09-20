9e0517f3037c0ccf087643a4b07ca82ebe46a36d
DenisTernovoy <96496534+DenisTernovoy@users.noreply.github.com>
2025-09-20 16:25:30 +0300

    Задание 2 готово

--- catalog/urls.py
+++ catalog/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+from apps import CatalogConfig
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('catalog.urls', namespace='catalog')),
+    path('catalog/', views.catalog, name='catalog'),
 ]
