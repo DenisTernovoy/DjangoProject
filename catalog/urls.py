e86960653619b9afd6393523f1ceb12df63d48fa
DenisTernovoy <96496534+DenisTernovoy@users.noreply.github.com>
2025-09-23 00:05:01 +0300

    Task 2 completed

--- catalog/urls.py
+++ catalog/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.urls import path
 from . import views
 from .apps import CatalogConfig
@@ -21,6 +22,6 @@ from .apps import CatalogConfig
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='catalog'),
+    path("", views.home, name="home"),
+    path("contacts/", views.contacts, name="catalog"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -3,15 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
 
         return HttpResponse(f"Спасибо, {name}! Мы обязательно Вам перезвоним.")
-    return render(request, 'contacts.html')
+    return render(request, "contacts.html")
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
