4b945c23612725bc7e5a48943c561721e0927488
DenisTernovoy <96496534+DenisTernovoy@users.noreply.github.com>
2025-09-23 11:18:13 +0300

    Task 7 completed

---
+++ catalog/management/commands/add_prodacts_categories.py
@@ -0,0 +1,19 @@
+import json
+from pathlib import Path
+
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+from catalog.models import Product, Category
+
+
+class Command(BaseCommand):
+    help = "Load test data categories and products from catalog_fixture"
+
+    def handle(self, *args, **kwargs):
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+
+        category, _ = Category.objects.get_or_create()
+
+        call_command("loaddata", "catalog_fixture.json")
+        self.stdout.write(self.style.SUCCESS("Successfully loaded data from fixture"))
