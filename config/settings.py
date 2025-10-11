11caff026effae97c2be6ef5f60f678298f5d4d9
DenisTernovoy <96496534+DenisTernovoy@users.noreply.github.com>
2025-10-11 12:49:22 +0300

    Исправлены замечания

--- config/settings.py
+++ config/settings.py
@@ -138,8 +138,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.mail.ru"
 EMAIL_PORT = 2525
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD_USER")
