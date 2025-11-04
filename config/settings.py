1bfe6054514e80825fe5260b0f207d1057328984
DenisTernovoy <96496534+DenisTernovoy@users.noreply.github.com>
2025-11-04 15:38:52 +0300

    Task 1 completed

--- config/settings.py
+++ config/settings.py
@@ -127,6 +127,16 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+CACHE_ENABLED = "True" == os.getenv("CACHE_ENABLED")
+
+if CACHE_ENABLED:
+    CASHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": os.getenv("LOCATION"),
+        }
+    }
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.mail.ru"
 EMAIL_PORT = 2525
